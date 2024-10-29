e5ab6daecf40cde7fed09080bf0b718211953156
TungNK01 <tung.nguyen@codegym.vn>
2024-10-29 22:07:14 +0700

    controller

--- src/main/java/com/codegym/c0424m1springoverview/controller/CustomerController.java
+++ src/main/java/com/codegym/c0424m1springoverview/controller/CustomerController.java
@@ -5,12 +5,15 @@ import com.codegym.c0424m1springoverview.service.CustomerServiceImpl;
 import com.codegym.c0424m1springoverview.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequestMapping("/customers")
 public class CustomerController {
 
     //private ICustomerService customerService = new CustomerServiceImpl();
@@ -32,10 +35,40 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/customers")
-    public String showList(HttpServletRequest request) {
+    @GetMapping("/list")
+    public String showListCustomers(HttpServletRequest request) {
         List<Customer> customers = customerService.findAll();
         request.setAttribute("customers", customers);
-        return "customers/list.jsp";
+        return "/customers/list";
     }
+
+    @GetMapping("/create")
+    public String showCreateForm() {
+        return "/customers/create";
+    }
+
+    /*@PostMapping("/create")
+    public String createCustomer(@RequestParam String name, @RequestParam String email, @RequestParam String address) {
+        int id = (int) (Math.random() * 10000);
+        Customer customer = new Customer(id, name, email, address);
+        customerService.save(customer);
+        return "redirect:/customers/list";
+    }*/
+
+    @PostMapping("/create")
+    public String createCustomerjahfjhafjahfjkahfjkadhfjahdfj(@ModelAttribute Customer customer) {
+        int id = (int) (Math.random() * 10000);
+        Customer customerObject = new Customer(id, customer.getName(), customer.getEmail(), customer.getAddress());
+        customerService.save(customerObject);
+        return "redirect:/customers/list";
+    }
+
+    @GetMapping("/find/{id}/{name}")
+    public String findCustomerById(@PathVariable int id, @PathVariable String name ) {
+        return"";
+    }
+
+
+
+
 }
--- src/main/webapp/WEB-INF/views/customers/list.jsp
+++ src/main/webapp/WEB-INF/views/customers/list.jsp
@@ -6,9 +6,7 @@
 </head>
 <body>
 <h1>Customers</h1>
-<p>
-  <a href="/customers?action=create">Create new customer</a>
-</p>
+
 <table border="1">
   <tr>
     <td>Name</td>
